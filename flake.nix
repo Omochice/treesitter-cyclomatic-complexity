80044e0c2de092a86c601bce64c229b1c2f0331d
Omochice <mochice.mls.ntl@gmail.com>
2026-03-23 23:41:18 +0900

    build: use nix

---
+++ flake.nix
@@ -0,0 +1,144 @@
+{
+  description = "sample description";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nur-packages = {
+      url = "github:Omochice/nur-packages";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+      flake-utils,
+      nur-packages,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            nur-packages.overlays.default
+          ];
+        };
+        treefmt = treefmt-nix.lib.evalModule pkgs (
+          { ... }:
+          let
+            rumdlConfig = (pkgs.formats.toml { }).generate "rumdl.toml" {
+              # keep-sorted start
+              MD004.style = "dash";
+              MD007.indent = 4;
+              MD007.style = "fixed";
+              MD041.enabled = false;
+              MD049.style = "underscore";
+              MD050.style = "asterisk";
+              MD055.style = "leading-and-trailing";
+              MD060.enabled = true;
+              MD060.style = "aligned";
+              global.length = 0;
+              # keep-sorted end
+            };
+          in
+          {
+            settings.global.excludes = [ ];
+            settings.formatter = {
+              # keep-sorted start block=yes
+              rumdl = {
+                command = "${pkgs.lib.getExe pkgs.rumdl}";
+                options = [
+                  "fmt"
+                  "--config"
+                  (toString rumdlConfig)
+                ];
+                includes = [ "*.md" ];
+              };
+              # keep-sorted end
+            };
+            programs = {
+              # keep-sorted start block=yes
+              keep-sorted.enable = true;
+              nixfmt.enable = true;
+              stylua.enable = true;
+              toml-sort.enable = true;
+              yamlfmt = {
+                enable = true;
+                settings = {
+                  formatter = {
+                    type = "basic";
+                    retain_line_breaks_single = true;
+                  };
+                };
+              };
+              # keep-sorted end
+            };
+          }
+        );
+        devPackages = rec {
+          # keep-sorted start block=yes
+          actions = with pkgs; [
+            actionlint
+            ghalint
+            zizmor
+          ];
+          # keep-sorted end
+          default = [
+            treefmt.config.build.wrapper
+          ]
+          ++ actions;
+        };
+      in
+      {
+        # keep-sorted start block=yes
+        checks = {
+          # keep-sorted start
+          actions =
+            pkgs.runCommand "check-actions"
+              {
+                buildInputs = with pkgs; [
+                  actionlint
+                  ghalint
+                  zizmor
+                ];
+                src = self;
+              }
+              ''
+                cd $src
+                actionlint .github/**/*.{yaml,yml}
+                ghalint run
+                zizmor .github/workflows .github/actions
+                touch $out
+              '';
+          formatting = treefmt.config.build.check self;
+          renovate =
+            pkgs.runCommand "validate-renovate-config"
+              {
+                buildInputs = with pkgs; [
+                  renovate
+                ];
+                src = self;
+              }
+              ''
+                cd $src
+                renovate-config-validator --strict renovate.json5
+                touch $out
+              '';
+          # keep-sorted end
+        };
+        devShells = pkgs.lib.pipe devPackages [
+          (pkgs.lib.attrsets.mapAttrs (name: buildInputs: pkgs.mkShell { inherit buildInputs; }))
+        ];
+        formatter = treefmt.config.build.wrapper;
+        # keep-sorted end
+      }
+    );
+}
